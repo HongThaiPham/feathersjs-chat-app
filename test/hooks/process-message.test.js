156d0932579bc48857356e2535086cd97abef1bf
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2019-03-15 15:32:43 +0700

    client

--- test/hooks/process-message.test.js
+++ test/hooks/process-message.test.js
@@ -1,27 +1,46 @@
+/* eslint-disable quotes */
 const assert = require('assert');
 const feathers = require('@feathersjs/feathers');
 const processMessage = require('../../src/hooks/process-message');
 
-describe('\'process-message\' hook', () => {
+describe("'process-message' hook", () => {
   let app;
 
   beforeEach(() => {
     app = feathers();
 
-    app.use('/dummy', {
-      async get(id) {
-        return { id };
+    app.use('/messages', {
+      async create(data) {
+        return data;
       }
     });
 
-    app.service('dummy').hooks({
-      before: processMessage()
+    app.service('messages').hooks({
+      before: {
+        before: processMessage()
+      }
     });
   });
 
-  it('runs the hook', async () => {
-    const result = await app.service('dummy').get('test');
-    
-    assert.deepEqual(result, { id: 'test' });
+  it('processes the message as expected', async () => {
+    // A user stub with just an `_id`
+    const user = { _id: 'test' };
+    // The service method call `params`
+    const params = { user };
+
+    // Create a new message with params that contains our user
+    const message = await app.service('messages').create(
+      {
+        text: 'Hi there',
+        additional: 'should be removed'
+      },
+      params
+    );
+
+    assert.equal(message.text, 'Hi there');
+    // `userId` was set
+    assert.equal(message.userId, 'test');
+    // `additional` property has been removed
+    assert.ok(!message.additional);
   });
 });
